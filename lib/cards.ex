e789378215c6bf0cc17bab7b702525bcbd276541
Rainer <rainer@fastcomm.com>
2019-03-15 12:07:26 +0200

    [Cards] First iteration of adding cards and suits

--- lib/cards.ex
+++ lib/cards.ex
@@ -15,6 +15,14 @@ defmodule Cards do
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+    
+    cards = for value <- values do
+      for suit <- suits do
+        "#{value} of #{suit}"
+      end
+    end
+
+    List.flatten(cards)
   end
 
   def shuffle(deck) do
